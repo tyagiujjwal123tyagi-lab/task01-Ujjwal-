f46b1549e8a93906c050a3e56cb1d8e98836f72e
tyagiujjwal123tyagi-lab <tyagiujjwal123tyagi@gmail.com>
2026-06-22 21:34:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Portfolio Loaded Successfully");
+
+document.getElementById("contactBtn")
+.addEventListener("click", function(){
+
+    alert(
+        "Thank you for visiting my portfolio!"
+    );
+
+});
